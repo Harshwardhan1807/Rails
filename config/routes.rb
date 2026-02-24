1df9889c451050bf7577dcb8eccbefff1add5783
Harshwardhan1807 <sharshu1807@gmail.com>
2026-02-24 12:39:35 +0530

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :channels
+  post "channels/:id/subscribe", to: "subscriptions#create", as: :channel_subscription
+  delete "channels/:id/unsubscribe", to: "subscriptions#destroy", as: :channel_unsubscription
 
   root "users#home"
 end
